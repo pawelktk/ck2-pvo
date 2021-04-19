a00fbc41df66ea8e8bae0bb3506df41be4d00fe3
Paweł Kłopotek <pawel.klopotek@gmail.com>
2021-04-19 18:41:44 +0200

    Localisation fix

--- PVO/common/defines/PVO_defines.lua
+++ PVO/common/defines/PVO_defines.lua
@@ -1,7 +1,7 @@
 	NDefines.NDiplomacy.DEMESNE_MAX_SIZE_PATRICIAN = 6.0			-- Extra Max Demesne Size for Patricians (Tier effects do no not apply to Patricians!)
 	
 	--NDefines.NEconomy.TRADE_POST_COST_INC_DIST = 0.016				-- The % increase in cost due to distance to closest connected province
-	NDefines.NEconomy.MAX_TRADE_POSTS_BASE = 1000
+	--NDefines.NEconomy.MAX_TRADE_POSTS_BASE = 1000
 	NDefines.NEconomy.MIN_TRADE_POSTS = 6							-- Minimum number of max trade posts per patrician
 	NDefines.NEconomy.PATRICIAN_FAMILY_SHARES_HEAD = 100				-- Monthly Patrician Income, number of shares for the Patrician himself
 	NDefines.NEconomy.PATRICIAN_FAMILY_SHARES_REST = 1				-- Monthly Patrician Income, number of shares for adult male dynasty members in the Patrician's court
