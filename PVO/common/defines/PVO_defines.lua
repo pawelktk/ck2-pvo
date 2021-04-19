83ce372ef32757b02bf01ec662847aca3fb0d2fa
Paweł Kłopotek <pawel.klopotek@gmail.com>
2021-04-19 15:44:47 +0200

    republic defines

---
+++ PVO/common/defines/PVO_defines.lua
@@ -0,0 +1,10 @@
+	NDefines.NDiplomacy.DEMESNE_MAX_SIZE_PATRICIAN = 6.0			-- Extra Max Demesne Size for Patricians (Tier effects do no not apply to Patricians!)
+	
+	--NDefines.NEconomy.TRADE_POST_COST_INC_DIST = 0.016				-- The % increase in cost due to distance to closest connected province
+	NDefines.NEconomy.MAX_TRADE_POSTS_BASE = 1000
+	NDefines.NEconomy.MIN_TRADE_POSTS = 6							-- Minimum number of max trade posts per patrician
+	NDefines.NEconomy.PATRICIAN_FAMILY_SHARES_HEAD = 100				-- Monthly Patrician Income, number of shares for the Patrician himself
+	NDefines.NEconomy.PATRICIAN_FAMILY_SHARES_REST = 1				-- Monthly Patrician Income, number of shares for adult male dynasty members in the Patrician's court
+	NDefines.NEconomy.PATRICIAN_INHERITANCE_FROM_RELATIVE_MULT = 1	-- When a Patrician inherits the wealth of a non-Patrician relative, this multiplier determines the part he actually gets
+	NDefines.NEconomy.FORT_CONSUMED_IN_SETTLEMENT_CONTRUCTION = 0		-- If set to 1 then fort holdings are consumed by the construction of another holding in the province while giving a discount to the construction cost in return.
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ck2-pvo
 Crusader Kings 2 Mod - Paul's Vanilla Overhaul
+Collection of mods updated to newest version of CK2. I am making this modpack for a personal use, but you can also use it and incorporate it into your own mod
+
+
+# Changes
