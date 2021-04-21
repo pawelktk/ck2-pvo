79be02202a69791d1c4428fdd6d9331dd9972a84
Paweł Kłopotek <pawel.klopotek@gmail.com>
2021-04-21 10:26:57 +0200

    QoL tweaks

--- PVO/common/defines/PVO_defines.lua
+++ PVO/common/defines/PVO_defines.lua
@@ -10,3 +10,6 @@
 
 	NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_STANDARD = 0	 	-- Chances that artifacts disappear on inheritance
 	NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_NO_HEIR = 0		-- Chances that artifacts disappear on inheritance when the dying character doesn't have an heir
+	
+	
+	NDefines.NCharacter.MAX_WARDS_PER_GUARDIAN = 64
